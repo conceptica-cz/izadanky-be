9077e456ff685ac07a1c3c73de167830c86eb414
Alexey Londkevich <londkevich@gmail.com>
2022-05-13 15:09:43 +0200

    Add celery queues.

--- scripts/start_worker.sh
+++ scripts/start_worker.sh
@@ -4,4 +4,16 @@
 ./scripts/wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT
 
 : ${WORKER_CONCURRENCY:=4}
-celery -A izadanky_web.celery worker -E --loglevel=info --concurrency=$WORKER_CONCURRENCY -n worker
+
+QUEUS=high_priority,low_priority
+NAME=worker
+
+while getopts 'Q:n:' c
+do
+    case $c in
+        Q) QUEUS=$OPTARG ;;
+        n) NAME=$OPTARG ;;
+    esac
+done
+
+celery -A izadanky_web.celery worker -E -Q $QUEUS --loglevel=info --concurrency=$WORKER_CONCURRENCY -n $NAME
